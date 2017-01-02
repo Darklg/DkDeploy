932f91a4186064b143d7f4f75501b13b83f5b5f8
Kevin <kevin@rocher-dev.com>
2017-01-02 17:26:10 +0100

    v 0.2.0
    * fix #3 - Reset old vars + Check for missing vars.
    * fix #4 - Exclusion in a variable.
    * fix #5 - Post deploy action.

--- README.md
+++ README.md
@@ -24,21 +24,24 @@ Create a file in the DkDeploy dir named config.sh
 #!/bin/bash
 
 # FTP HOST
-HOST="myhost.com"
+DKDEPLOY_HOST="myhost.com"
 # FTP USERNAME
-LOGIN="username"
+DKDEPLOY_LOGIN="username"
 # FTP PASSWORD
-PASSWORD="password"
+DKDEPLOY_PASSWORD="password"
 # CUSTOM DIR TO DEPLOY
-BASEDIR='prod/'
+DKDEPLOY_BASEDIR='prod/'
 # LOCAL ROOT PROJECT DIR
-LOCALDIR="/mywebsites/project/htdocs/${BASEDIR}"
+DKDEPLOY_LOCALDIR="/mywebsites/project/htdocs/${BASEDIR}"
 # DISTANT ROOT PROJECT DIR
-REMOTEDIR="/var/www/project/htdocs/${BASEDIR}"
+DKDEPLOY_REMOTEDIR="/var/www/project/htdocs/${BASEDIR}"
+# CUSTOM EXCLUSION RULES
+DKDEPLOY_EXCLUDECUSTOM="--exclude-glob *custom_cache/ --exclude-glob *custom_cache_html/";
 ```
 
 ### Custom files exclusions
 
+If you don't want to use the DKDEPLOY_EXCLUDECUSTOM var,
 Create a file in the DkDeploy dir named exclude-custom.txt.
 Set one filename/dirname per line and prefix lines with *.
 
@@ -46,3 +49,8 @@ Set one filename/dirname per line and prefix lines with *.
 *myfile.txt
 *myconfig.txt
 ```
+
+### Custom post deploy actions.
+
+Create a file in the DkDeploy dir named post-deploy.sh
+It will be triggered after deploy.
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###################################
-## DK Deploy v 0.1.0
+## DK Deploy v 0.2.0
 ## License MIT
 ## https://github.com/Darklg/DkDeploy
 
@@ -9,13 +9,17 @@
 ## LOAD SCRIPT DIR
 ###################################
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+DKDEPLOY_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 ###################################
-## LOAD HELPERS
+## LOAD DEPENDENCIES
 ###################################
 
-. "${SCRIPT_DIR}/inc/helper.sh";
+# Helper functions
+. "${DKDEPLOY_SCRIPTDIR}/inc/helper.sh";
+
+# Reset user vars
+. "${DKDEPLOY_SCRIPTDIR}/inc/reset.sh";
 
 ###################################
 ## CHECK REQUIREMENTS
@@ -32,20 +36,26 @@ fi
 ## LOAD INFOS
 ###################################
 
-. "${SCRIPT_DIR}/config.sh";
+. "${DKDEPLOY_SCRIPTDIR}/config.sh";
+
+# Check infos
+if [ -z ${DKDEPLOY_HOST+x} ] || [ -z ${DKDEPLOY_LOGIN+x} ] || [ -z ${DKDEPLOY_PASSWORD+x} ] || [ -z ${DKDEPLOY_LOCALDIR+x} ] || [ -z ${DKDEPLOY_REMOTEDIR+x} ];then
+    echo '- /!\ The config file is invalid. Please define all the necessary vars : DKDEPLOY_HOST, DKDEPLOY_LOGIN, DKDEPLOY_PASSWORD, DKDEPLOY_LOCALDIR, DKDEPLOY_REMOTEDIR.';
+    return;
+fi;
 
 ###################################
 ## CHECK IF PUSH IS POSSIBLE
 ###################################
 
-cd "${LOCALDIR}";
+cd "${DKDEPLOY_LOCALDIR}";
 
 # On master branch
 # http://stackoverflow.com/a/1593487
-branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
-branch_name="(unnamed branch)"
-branch_name=${branch_name##refs/heads/}
-if [ $branch_name != 'master' ]; then
+DKDEPLOY_CURRENT_BRANCH="$(git symbolic-ref HEAD 2>/dev/null)" ||
+DKDEPLOY_CURRENT_BRANCH="(unnamed branch)"
+DKDEPLOY_CURRENT_BRANCH=${DKDEPLOY_CURRENT_BRANCH##refs/heads/}
+if [ $DKDEPLOY_CURRENT_BRANCH != 'master' ]; then
     echo '- /!\ You must be on the master branch to deploy.';
     return;
 fi;
@@ -56,16 +66,16 @@ if [[ -n $(git status -s) ]]; then
     return;
 fi;
 
-cd "${SCRIPT_DIR}";
+cd "${DKDEPLOY_SCRIPTDIR}";
 
 ###################################
 ## DRY RUN ?
 ###################################
 
-ENABLEDRYRUN='--dry-run';
-read -p "Disable Dry run ? [y/N] : " disable_dry_run
-if [[ $disable_dry_run == 'y' ]]; then
-    ENABLEDRYRUN='';
+DKDEPLOY_ENABLEDRYRUN='--dry-run';
+read -p "Disable Dry run ? [y/N] : " tmp__disable_dry_run
+if [[ $tmp__disable_dry_run == 'y' ]]; then
+    DKDEPLOY_ENABLEDRYRUN='';
     echo '- Dry run is *disabled*. Changes *will* be applied.';
 else
     echo '- Dry run is *enabled*. Changes *will not* be applied.';
@@ -75,12 +85,14 @@ fi;
 ## LOAD EXCLUDES
 ###################################
 
-EXCLUDELIST=$(sed 's/^/--exclude-glob /' exclude-lists/base.txt | tr '\n' ' ');
-EXCLUDEWP=$(sed 's/^/--exclude-glob /' exclude-lists/wordpress.txt | tr '\n' ' ');
-EXCLUDECUSTOM='';
+DKDEPLOY_EXCLUDELIST=$(sed 's/^/--exclude-glob /' exclude-lists/base.txt | tr '\n' ' ');
+DKDEPLOY_EXCLUDEWP=$(sed 's/^/--exclude-glob /' exclude-lists/wordpress.txt | tr '\n' ' ');
+if [ -z ${DKDEPLOY_EXCLUDECUSTOM+x} ]; then
+    DKDEPLOY_EXCLUDECUSTOM='';
+fi
 if [[ -f 'exclude-custom.txt' ]];then
     echo '- Loading custom exclusion rules.';
-    EXCLUDECUSTOM=$(sed 's/^/--exclude-glob /' exclude-custom.txt | tr '\n' ' ');
+    DKDEPLOY_EXCLUDECUSTOM=$(sed 's/^/--exclude-glob /' exclude-custom.txt | tr '\n' ' ');
 fi;
 
 ###################################
@@ -91,22 +103,30 @@ fi;
 sleep 2;
 
 lftp -c "set ftp:list-options -a;
-open ftp://$LOGIN:$PASSWORD@$HOST;
-lcd $LOCALDIR;
-cd $REMOTEDIR;
+open ftp://$DKDEPLOY_LOGIN:$DKDEPLOY_PASSWORD@$DKDEPLOY_HOST;
+lcd $DKDEPLOY_LOCALDIR;
+cd $DKDEPLOY_REMOTEDIR;
 mirror \
 --reverse \
 --only-newer \
 --verbose \
-$ENABLEDRYRUN \
-$EXCLUDELIST \
-$EXCLUDEWP \
-$EXCLUDECUSTOM \
+$DKDEPLOY_ENABLEDRYRUN \
+$DKDEPLOY_EXCLUDELIST \
+$DKDEPLOY_EXCLUDEWP \
+$DKDEPLOY_EXCLUDECUSTOM \
 ";
 
 ###################################
 ## CONFIRM
 ###################################
 
+if [[ -f 'post-deploy.sh' ]];then
+    echo '- Loading custom post deploy actions.';
+    . "post-deploy.sh";
+fi;
+
 echo '- Deploy is done !';
-cd "${LOCALDIR}";
+cd "${DKDEPLOY_LOCALDIR}";
+
+# Reset user vars
+. "${DKDEPLOY_SCRIPTDIR}/inc/reset.sh";
